34049d2d01502662a0db8e19a22aea272b18ab1b
Kyewon <pkw3136@naver.com>
2020-05-17 01:17:40 +0900

    paddle width and height added

--- brick-breaker-master/src/Paddle.java
+++ brick-breaker-master/src/Paddle.java
@@ -37,19 +37,21 @@ public class Paddle extends Structure implements Constants {
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(color);
-		g.fillRect(x , y, width, height);
+		g.fillRect(x, y, width, height);
 	}
 
 	public void movePaddle() {
 		x = getX();
-		y = Board.FrameHeight -13;
+		y = Board.FrameHeight-13;
+		width = (int)((70.0/486.0)*Board.FrameWidth);
+		height = (int)((10.0/486.0)*Board.FrameHeight);
     }
 	
 	//Places the paddle back in starting position at center of screen(하단 바 리셋) 
 	public void reset() 
 	{
 		x = (Board.FrameWidth/2)-(Main.PADDLE_WIDTH/2);
-		y = Board.FrameHeight / 8 * 7;
+		y = Board.FrameHeight -13;
 
 		setWidth(Main.PADDLE_WIDTH); //하단 바 크기 초기화
 		setColor(Color.BLACK);//하단 바 색상 BLACK으로 초기화
