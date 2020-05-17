264e05c8a2d0086189d3fafa9dba512041c7ead6
ykbsuper <ykbsuper@naver.com>
2020-05-17 22:54:41 +0900

    fix some function

--- brick-breaker-master/src/Paddle.java
+++ brick-breaker-master/src/Paddle.java
@@ -1,12 +1,12 @@
 /*
 *    Brick Breaker, Version 1.2
 *    By Ty-Lucas Kelley
-*	
-*	 **LICENSE**
+*   
+*    **LICENSE**
 *
-*	 This file is a part of Brick Breaker.
+*    This file is a part of Brick Breaker.
 *
-*	 Brick Breaker is free software: you can redistribute it and/or modify
+*    Brick Breaker is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
 *    the Free Software Foundation, either version 3 of the License, or
 *    (at your option) any later version.
@@ -28,57 +28,58 @@ import java.awt.*;
 //Class definition
 public class Paddle extends Structure implements Constants {
 
-	//Constructor
-	public Paddle(int x , int y, int width, int height, Color color) {
-		super(x, y, width, height, color);
-	}
+   //Constructor
+   public Paddle(int x , int y, int width, int height, Color color) {
+      super(x, y, width, height, color);
+   }
 
-	//Draws the paddle
-	@Override
-	public void draw(Graphics g) {
-		g.setColor(color);
-		g.fillRect(x , y, width, height);
-	}
+   //Draws the paddle
+   @Override
+   public void draw(Graphics g) {
+      g.setColor(color);
+      g.fillRect(x, y, width, height);
+   }
 
-	public void movePaddle() {
-		x = getX();
-		y = Board.FrameHeight -13;
+   public void movePaddle() {
+      x = getX();
+      y = Board.FrameHeight -13;
     }
-	
-	//Places the paddle back in starting position at center of screen(하단 바 리셋) 
-	public void reset() 
-	{
-		x = (Board.FrameWidth/2)-(Main.PADDLE_WIDTH/2);
-		y = Board.FrameHeight / 8 * 7;
+   
+   //Places the paddle back in starting position at center of screen(하단 바 리셋) 
+   public void reset() 
+   {
+      x = (Board.FrameWidth/2)-(Main.PADDLE_WIDTH/2);
+      y = Board.FrameHeight -13;
+      width = (int)((70.0/486.0)*Board.FrameWidth);
+      height = (int)((10.0/486.0)*Board.FrameHeight);
+      setWidth(Main.PADDLE_WIDTH); //하단 바 크기 초기화
+      setColor(Color.BLACK);//하단 바 색상 BLACK으로 초기화
+      Board.reverse = false;//리버스 모드 비활성화
+   }
+   //하단 바를 왼쪽으로 이동(숫자를 조절해 하단 바의 속도 조절)
+   public void moveLeft() {
+      x -= 2;
+   }
+   //하단바를 오른쪽으로 이동(숫자를 조절해 하단 바의 속도 조절)
+   public void moveRight() {
+      x += 2;
+   }
+   //Checks if the ball hit the paddle
+   public boolean hitPaddle(int ballX, int ballY) {
+      if ((ballX >= x) && (ballX <= x + width) && ((ballY >= y) && (ballY <= y + height))) {
+         return true;
+      }
+      return false;
+   }
 
-		setWidth(Main.PADDLE_WIDTH); //하단 바 크기 초기화
-		setColor(Color.BLACK);//하단 바 색상 BLACK으로 초기화
-		Board.reverse = false;//리버스 모드 비활성화
-	}
-	//하단 바를 왼쪽으로 이동(숫자를 조절해 하단 바의 속도 조절)
-	public void moveLeft() {
-		x -= 2;
-	}
-	//하단바를 오른쪽으로 이동(숫자를 조절해 하단 바의 속도 조절)
-	public void moveRight() {
-		x += 2;
-	}
-	//Checks if the ball hit the paddle
-	public boolean hitPaddle(int ballX, int ballY) {
-		if ((ballX >= x) && (ballX <= x + width) && ((ballY >= y) && (ballY <= y + height))) {
-			return true;
-		}
-		return false;
-	}
-
-	//Resizes the paddle if it touches an item, then returns true or false
-	public boolean caughtItem(Item i) {
-		if ((i.getX() < x + width) && (i.getX() + i.getWidth() > x) && (y == i.getY() || y == i.getY() - 1)) {
-			i.resizePaddle(this); //하단 바 크기 변경
-			i.changeBallSpeed(this); //공 속도 변경
-			i.reverseMode(this); //리버스 모드
-			return true;
-		}
-		return false;
-	}
+   //Resizes the paddle if it touches an item, then returns true or false
+   public boolean caughtItem(Item i) {
+      if ((i.getX() < x + width) && (i.getX() + i.getWidth() > x) && (y == i.getY() || y == i.getY() - 1)) {
+         i.resizePaddle(this); //하단 바 크기 변경
+         i.changeBallSpeed(this); //공 속도 변경
+         i.reverseMode(this); //리버스 모드
+         return true;
+      }
+      return false;
+   }
 }
