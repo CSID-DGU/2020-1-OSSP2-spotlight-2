92bd06b7c75b193de13f71eef67a072144f0fa99
Kyewon <pkw3136@naver.com>
2020-05-19 22:06:58 +0900

    background

--- brick-breaker-master/src/Brick.java
+++ brick-breaker-master/src/Brick.java
@@ -49,15 +49,8 @@ public class Brick extends Structure implements Constants, ImageObserver {
 	private Color[][] colors = {blueColors, redColors, purpleColors, yellowColors, pinkColors, grayColors, greenColors};
 
 	//블록 이미지 img에 저장
-<<<<<<< HEAD
-<<<<<<< HEAD
-	ImageIcon icon = new ImageIcon("./img/virus2.png");
-=======
 	ImageIcon icon = new ImageIcon("./img/ballvirus.png");
->>>>>>> 880063c6c28e6d07d904d44881c64e126ce123ee
-=======
-	ImageIcon icon = new ImageIcon("./img/ballvirus.png");
->>>>>>> yoon
+
 	Image img = icon.getImage();
 	
 	
--- brick-breaker-master/src/Main.java
+++ brick-breaker-master/src/Main.java
@@ -47,7 +47,7 @@ public class Main extends JFrame implements Constants {
 			e.printStackTrace();
 		}
 
-		icon = new ImageIcon("./img/Virus - 33131.gif");
+		icon = new ImageIcon("./img/earth3.gif");
 		
 		frame = new JFrame("Virus Breaker");
 		frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
